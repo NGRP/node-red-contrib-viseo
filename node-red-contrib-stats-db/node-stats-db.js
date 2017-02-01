b76ed06602460844e11c3517e23145f0fa4521f2
Matthias Brunet <matthias.brunet@viseo.com>
2017-02-01 16:35:34 +0100

    inserts added to node

--- node-red-contrib-stats-db/node-stats-db.js
+++ node-red-contrib-stats-db/node-stats-db.js
@@ -2,16 +2,50 @@
 
 const sqlite3 = require('sqlite3').verbose();
 
+const insertIntentStats = (node, result) => {
+    node.statsDb.run('INSERT INTO intents VALUES($intent, $question)', {
+        $intent: result.metadata && result.metadata.intentName,
+        $question: result.resolvedQuery
+    });
+};
+
+const insertUserStats = (node, user) => {
+    node.statsDb.run('INSERT INTO users VALUES($fbId, $lastSeen)', {
+        $fbId: user.id,
+        $lastSeen: user.mdate
+    });
+};
+
+const insertHotelStats = (node, hotel) => {
+    node.statsDb.run('INSERT INTO hotels VALUES($rid, $name)', {
+        $rid: hotel.code,
+        $name: hotel.name
+    });
+};
+
 const inputHandler = (node, data, config) => {
+    if (data.payload && data.payload.result) {
+        insertIntentStats(node, data.payload.result);
+    }
+
+    if (data.user) {
+        insertUserStats(node, data.user);
+    }
+
+    if (data.hotel) {
+        insertHotelStats(node, data.hotel);
+    }
+
     node.send(data);
 };
 
 module.exports = function (RED) {
     const register = function (config) {
         RED.nodes.createNode(this, config);
-        let node = this;
+        const db = new sqlite3.Database('./dbstats');
+        this.statsDb = db;
 
-        const db = new sqlite3.Database('./dbtest');
+        let node = this;
 
         this.on('input', (data) => {
             inputHandler(node, data, config);
