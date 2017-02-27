26e5b3a1fc7a52f231619e3f8d555814eaeb099c
Matthias Brunet <matthias.brunet@viseo.com>
2017-02-27 15:53:46 +0000

    edit fb menus

--- node-red-contrib-botbuilder/nodes/channel/msbot-fb-greeting.js
+++ node-red-contrib-botbuilder/nodes/channel/msbot-fb-greeting.js
@@ -9,6 +9,46 @@ let info  = console.log;
 let error = console.log;
 
 // --------------------------------------------------------------------------
+//  FACEBOOK API
+// --------------------------------------------------------------------------
+
+const getPageToken = () => {
+    if (!CONFIG || !CONFIG.facebook || CONFIG.facebook.pageToken) return;
+    return CONFIG.facebook.pageToken;
+}
+
+let GREETING_MSG = {
+    "setting_type":"greeting",
+    "greeting":{ "text":"Your greeting text here." }
+}
+
+const GET_STARTED = {
+    "setting_type":"call_to_actions",
+    "thread_state":"new_thread",
+    "call_to_actions":[{ "payload":"getstarted" }]
+}
+
+const PERSISTANT_MENU = {
+    "setting_type" : "call_to_actions",
+    "thread_state" : "existing_thread",
+    "call_to_actions":[]
+}
+
+const facebookThreadAPI = (json) => {
+    console.log(PERSISTANT_MENU);
+    let token = getPageToken();
+    if (!token) return;
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+token,
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        form: json
+    },
+    function (err, response, body) { error(err.message, err); });
+}
+
+// --------------------------------------------------------------------------
 //  NODE-RED
 // --------------------------------------------------------------------------
 
@@ -34,6 +74,7 @@ const input = (node, data, config) => {
 }
 
 const start = (RED, node, config) => {
+    console.log(config);
     for (let i = 1; i <= 5; i++) {
         if (config['menu' + i] && config['action' + i] && config['value' + i]) {
             const menu = {
@@ -45,6 +86,7 @@ const start = (RED, node, config) => {
             PERSISTANT_MENU.call_to_actions.push(menu);
         }
     }
+    facebookThreadAPI(PERSISTANT_MENU);
     RED.httpAdmin.post("/fb-greeting/:id", function(req,res) {
         var node = RED.nodes.getNode(req.params.id);
         if (node == null) { return res.sendStatus(404); }
@@ -55,45 +97,3 @@ const start = (RED, node, config) => {
     });
 }
 
-// --------------------------------------------------------------------------
-//  FACEBOOK API
-// --------------------------------------------------------------------------
-
-const getPageToken = () => {
-    if (!CONFIG || !CONFIG.facebook || CONFIG.facebook.pageToken) return;
-    return CONFIG.facebook.pageToken;
-}
-
-let GREETING_MSG = {
-    "setting_type":"greeting",
-    "greeting":{ "text":"Your greeting text here." }
-}
-
-const GET_STARTED = {
-    "setting_type":"call_to_actions",
-    "thread_state":"new_thread",
-    "call_to_actions":[{ "payload":"getstarted" }]
-}
-
-const PERSISTANT_MENU = {
-    "setting_type" : "call_to_actions",
-    "thread_state" : "existing_thread",
-    "call_to_actions":[{
-        "type":"postback",
-        "title":"Admin Reset",
-        "payload":"getstarted"
-    }]
-}
-
-const facebookThreadAPI = (json) => {
-    let token = getPageToken();
-    if (!token) return;
-
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+token,
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        form: json
-    },
-    function (err, response, body) { error(err.message, err); });
-}
