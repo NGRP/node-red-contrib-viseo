190fadf61ed70f0a432d68de63d882fdb8c28cac
Jean-Philippe Encausse <Jp@encausse.net>
2017-03-19 18:58:59 +0100

    Add feature "once" to random node
    call at least every output before calling the same one

--- node-red-contrib-random/node-random.js
+++ node-red-contrib-random/node-random.js
@@ -1,4 +1,6 @@
 
+const helper    = require('node-red-viseo-helper');
+
 // --------------------------------------------------------------------------
 //  NODE-RED
 // --------------------------------------------------------------------------
@@ -16,6 +18,23 @@ const input = (node, data, config) => {
     let out  = new Array(parseInt(config.outputs));
     let rand = Math.round(Math.random() * (out.length-1));
 
+    if (config.topic){
+        let order = helper.getByString(data, config.topic, undefined);
+        if (!order || order.length <= 0){ order = [];
+            for(let i=0; i < config.outputs ; i++){ order.push(i); }
+            shuffle(order);
+            helper.setByString(data, config.topic, order);
+        }
+        rand = order.pop();
+    }
+
     out[rand] = data;
     node.send(out);
+}
+
+const shuffle = (a) => {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
 }
