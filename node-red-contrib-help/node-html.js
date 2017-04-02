ce6c3302d69c341cf24de6039c7582bbb50c349e
Jean-Philippe Encausse <Jp@encausse.net>
2017-04-02 20:51:45 +0200

    First introduce node-red-contrib-help
    This node display an HTML content

---
+++ node-red-contrib-help/node-html.js
@@ -0,0 +1,20 @@
+
+const helper    = require('node-red-viseo-helper');
+
+// --------------------------------------------------------------------------
+//  NODE-RED
+// --------------------------------------------------------------------------
+
+module.exports = function(RED) {
+    const register = function(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.on('input', (data)  => { input(node, data, config)  });
+    }
+    RED.nodes.registerType("help-html", register, {});
+}
+
+const input = (node, data, config) => {
+    node.send(data);
+}
+
