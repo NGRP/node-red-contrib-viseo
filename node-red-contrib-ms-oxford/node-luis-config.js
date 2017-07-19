db922f20bbf3f9ec4b989e74d9833e9b1540fbeb
To-Thi Hoang <to-thi.hoang@viseo.com>
2017-07-19 15:08:19 +0200

    clean host

--- node-red-contrib-ms-oxford/node-luis-config.js
+++ node-red-contrib-ms-oxford/node-luis-config.js
@@ -3,6 +3,9 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         this.name = config.name;
         this.host = config.host || "";
+        this.host = this.host.replace(/^https?:\/\//, '');
+        this.host = this.host.replace(/\/$/, '');
+
     }
     RED.nodes.registerType("ms-luis-config", register, {
     	credentials: {
