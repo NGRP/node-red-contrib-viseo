7df558fa29f757f2c60c163acfe80e7c41f63fe1
Klervix <klervi.menoret@gmail.com>
2017-07-31 09:43:04 +0200

    Test age_range

--- node-red-contrib-botbuilder/nodes/channel/msbot-fb-profile.js
+++ node-red-contrib-botbuilder/nodes/channel/msbot-fb-profile.js
@@ -78,7 +78,7 @@ const getPageToken = (node) => {
 }
 
 const URL = "https://graph.facebook.com/v2.8/";
-const QS  = "?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=";
+const QS  = "?fields=first_name,last_name,profile_pic,locale,timezone,gender,age_rang,birthday&access_token=";
 
 const getFBProfile = exports.getUserProfile = (node, uid, config, callback) => {
     if (undefined === uid) return callback();
