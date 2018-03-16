db5f20045a789f3149e8fac6e3af73af62da200b
Klervix <klervi.menoret@gmail.com>
2018-03-16 18:13:35 +0100

    Google Maps api

---
+++ node-red-contrib-google-maps/google-places.js
@@ -0,0 +1,89 @@
+
+const helper = require('node-red-viseo-helper');
+
+// --------------------------------------------------------------------------
+//  NODE-RED
+// --------------------------------------------------------------------------
+
+module.exports = function(RED) {
+    const register = function(config) {
+        RED.nodes.createNode(this, config);
+        let node = this;
+
+        start(RED, node, config);
+        this.on('input', (data) => { input(node, data, config);});
+        this.on('close', (done) => { stop(done); });
+    }
+    RED.nodes.registerType("google-places", register, {});
+}
+
+
+function start(RED, node, config) {
+    node.status({
+        fill:  "red", 
+        shape: "ring",
+        text:  'Missing credential'
+    })
+
+    var conf   = (config.auth) ? RED.nodes.getNode(config.auth) : {};
+    if (conf.credentials) {
+        config.key = conf.credentials.key;
+        node.status({});
+    }
+    
+}
+
+function input (node, data, config) {
+
+    let action  = config.action  || "search",
+        request = config.request || "textsearch",
+        output  = config.output  || "payload",
+        param   = config.parameters;
+
+    let outloc = (config.outputType === 'global') ? node.context().global : data;
+        
+    let parameters = {},
+        keys = Object.keys(param);
+
+    for (let i=0; i<keys.length; i++) {
+        var key = keys[i];
+        if (!param[key].value) continue;
+        let val = param[key].value;
+        if (param[key].typedInput === 'msg')      val = helper.getByString(data, param[key].value);
+        else if (param[key].typedInput === 'num') val = String(param[key].value);
+        else if (param[key].value === "true")     val = true;
+        else if (param[key].value === "false")    val = false;
+
+        parameters[key] = val;
+    }
+
+    const google = require('@google/maps').createClient({ key: config.key });
+
+    function cb(err, response) {
+        if (err) {
+            node.warn(err);
+            return node.send(data);
+        }
+
+        helper.setByString(outloc, output, response.json);
+        return node.send(data);
+    }
+
+    if (action === "search") {
+        if (request === "textsearch") google.places(parameters, cb);
+        else google.placesNearby(parameters, cb);
+    }
+
+    if (action === "autocomp") {
+        if (request === "autocomplete") google.placesAutoComplete(parameters, cb);
+        else  google.placesQueryAutoComplete(parameters, cb);
+    }
+
+    if (action === "details")  google.place(parameters, cb);
+    if (action === "photos") google.placesPhoto(parameters, cb);
+
+}
+
+const stop = (done) => {
+    done();
+};
