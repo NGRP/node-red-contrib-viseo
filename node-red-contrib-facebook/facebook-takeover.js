530b63bdb76de445bc46fb2427adb3e8c90c2ad9
To-Thi Hoang <to-thi.hoang@viseo.com>
2018-04-05 14:16:11 +0200

    added facebook takeover

---
+++ node-red-contrib-facebook/facebook-takeover.js
@@ -0,0 +1,69 @@
+const helper  = require('node-red-viseo-helper');
+const request = require('request');
+
+// --------------------------------------------------------------------------
+//  NODE-RED
+// --------------------------------------------------------------------------
+
+
+module.exports = function(RED) {
+    const register = function(config) {
+        RED.nodes.createNode(this, config);
+        let node = this;
+
+        this.config = RED.nodes.getNode(config.config);
+
+        start(RED, node, config);
+
+        this.on('input', (data) => { input(node, data, config)  });
+    }
+    RED.nodes.registerType("facebook-takeover", register);
+}
+
+
+const start = (RED, node, config) => {
+
+}
+
+const input = (node, data, config) => {
+    
+
+    let userId       = config.userId || 'user.id';
+        userId       = helper.getByString(data, userId, userId);
+
+    let metadata     = helper.getByString(data, config.metadata, config.metadata);
+
+
+    request(
+        {
+            method: "POST",
+            url: 'https://graph.facebook.com/v2.6/me/take_thread_control?access_token='+node.config.credentials.token,
+            headers: {'Content-Type': 'application/json'},
+            body: {
+                recipient : { "id" : userId },
+                metadata : metadata
+            },
+            json: true
+        }, function(err, response, body) {
+
+
+            if(err) {
+                console.log(err)
+                node.send([undefined, data]);
+            }
+
+            try {
+                if(body.success) {
+                    return node.send([data, undefined]);
+                }
+            } catch(ex) {
+                console.log(ex)
+            }
+
+            node.send([undefined, data]);
+        }
+    );
+
+
+}
+
