c185abb82ec01eb347fdb5dce7b333e95ae58743
Klervix <klervi.menoret@gmail.com>
2018-04-11 09:17:20 +0200

    V. 0.5.10

--- node-red-contrib-botbuilder/nodes/channel/msbot-fb-greeting.js
+++ node-red-contrib-botbuilder/nodes/channel/msbot-fb-greeting.js
@@ -1,5 +1,4 @@
-const request = require('request');
-
+const rp = require('request-promise');
 
 // --------------------------------------------------------------------------
 //  LOGS
@@ -19,43 +18,37 @@ module.exports = function(RED) {
         start(RED, node, config);
 
         this.config = RED.nodes.getNode(config.config);
-
         this.on('input', (data)  => { input(node, data, config)  });
     }
     RED.nodes.registerType("fb-greeting", register, {});
 }
 
+
 const input = (node, data, config) => { 
-    if (!config.greeting) { return; }
-    facebookThreadAPI(node, config, GET_STARTED);
 
-    // Set greeting message
-    GREETING_MSG.greeting.text = config.greeting
-    facebookThreadAPI(node, config, GREETING_MSG);
-
-    if (!config.buttons) { return; }
-
-    // Set Persistant
-    // https://developers.facebook.com/docs/messenger-platform/thread-settings/domain-whitelisting
-    PERSISTANT_MENU.call_to_actions = []
-    for (let button of config.buttons){
-        let btn = {
-            "type"   : button.action,
-            "title"  : button.title,
-            "payload": button.value
+    if (!config.greeting) return;
+    
+    let sendData = {
+        "greeting" : [{
+            "locale":"default",
+            "text": config.greeting
+        }],
+        "get_started": {
+            "payload":"getstarted"
         }
-        if (btn.type == 'web_url_compact'){
-            btn.type = 'web_url';
-            btn.webview_height_ratio = "full";
-            btn.messenger_extensions = true;
-        }
-        if (btn.type == 'web_url'){
-            btn.url = btn.payload
-            delete btn.payload;
-        }
-        PERSISTANT_MENU.call_to_actions.push(btn)
     }
-    facebookThreadAPI(node, config, PERSISTANT_MENU);
+
+
+    // Set Persistant menu
+    if (config.buttons) sendData.persistent_menu = [{
+        "locale":"default",
+        "composer_input_disabled": false,
+        "call_to_actions": buildObject(config.buttons) 
+    }];
+
+    facebookAPI(node, config, sendData)
+    .then(function (res)  { node.warn(res) })
+    .catch(function (err) { node.error(err); });
 }
 
 const start = (RED, node, config) => {
@@ -75,44 +68,64 @@ const start = (RED, node, config) => {
 
 const getPageToken = (node) => {
 
-    if (CONFIG && CONFIG.facebook && CONFIG.facebook.pageToken)
-        return CONFIG.facebook.pageToken;
-
+    if (CONFIG && CONFIG.facebook && CONFIG.facebook.pageToken) return CONFIG.facebook.pageToken;
     return node.config.credentials.token;
 }
 
-let GREETING_MSG = {
-    "setting_type":"greeting",
-    "greeting":{ "text":"Your greeting text here." }
-}
-
-const GET_STARTED = {
-    "setting_type":"call_to_actions",
-    "thread_state":"new_thread",
-    "call_to_actions":[{ "payload":"getstarted" }]
-}
-
-const PERSISTANT_MENU = {
-    "setting_type" : "call_to_actions",
-    "thread_state" : "existing_thread",
-    "call_to_actions":[{
-        "type":"postback",
-        "title":"Admin Reset",
-        "payload":"getstarted"
-    }]
-}
-
-const facebookThreadAPI = (node, config, json) => { 
+const facebookAPI = (node, config, json) => { 
     let token = getPageToken(node);
     if (!token) return;
 
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+token,
+    var req = {
+        uri: 'https://graph.facebook.com/v2.6/me/messenger_profile?access_token='+token,
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        form: json
-    },
-    function (err, response, body) { 
-       if (err) node.error(err); else node.warn(body);
-    });
+        body: json,
+        json: true
+    }
+    return rp(req);
+}
+
+const buildObject = (buttons) => {
+    if (buttons.length < 1) return [];
+
+    var array = [];
+    var tmp = [];
+    var cache = {0:[]}
+
+    for (let i=0; i<buttons.length; i++) {
+        let btn = {
+            "type"    :  buttons[i].action,
+            "title"   :  buttons[i].title,
+            "payload" :  buttons[i].value
+        }
+
+        switch(buttons[i].action) {
+            case "nested":
+                delete btn.payload;
+                btn.call_to_actions = [];
+                break;
+            case "web_url":
+                delete btn.payload;
+                btn.url = btn.payload
+                break;
+            case "web_url_compact":
+                btn.type = 'web_url';
+                btn.webview_height_ratio = "full";
+                btn.messenger_extensions = true;
+                break;
+        }
+
+        if (buttons[i].level === 0) {
+            array.push(btn);
+            cache[0] = btn;
+            continue;
+        }
+
+        if (!cache[buttons[i].level-1].call_to_actions) continue;
+        cache[buttons[i].level-1].call_to_actions.push(btn);
+        cache[buttons[i].level] = btn;
+
+    }
+    console.log(array)
+    return array;
 }
---
+++ node-red-contrib-botbuilder/nodes/offline/lib/index.js
@@ -0,0 +1,30 @@
+$(function() {
+    
+    $(window).load(function() {
+        var $bot = $('#bot');
+        var connector = new BotChat.DirectLine({
+            secret: "viseo",
+            domain: "http://"+document.location.hostname+":3000/directline",
+            webSocket: false // defaults to true
+        });
+
+        BotChat.App({
+            botConnection: connector,
+            user: { 
+                id: $bot.data('user'),
+                name: "User"
+            },
+            bot: { id: "viseo" },
+            resize: 'detect'
+        }, $bot.get(0));
+
+        connector.postActivity({
+            from: { id: $bot.data('user')},
+            type: 'message',
+            text: $bot.data('start')
+        }).subscribe(
+            id => {},
+            error => console.log("Error posting activity", error)
+        );
+    })
+});
