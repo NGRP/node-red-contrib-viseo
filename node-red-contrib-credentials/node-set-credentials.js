043ced166a3453aa315fc216d1f3363602366906
Klervix <klervi.menoret@gmail.com>
2018-10-22 14:59:18 +0200

    V 0.0.2

--- node-red-contrib-credentials/node-set-credentials.js
+++ node-red-contrib-credentials/node-set-credentials.js
@@ -6,35 +6,26 @@ module.exports = function(RED) {
     const register = function(config) {
         RED.nodes.createNode(this, config);
         let node = this;
+        config.rules = RED.nodes.getNode(config.rules);
         this.on('input', (data)  => { input(RED, node, data, config)  });
     }
-    RED.nodes.registerType("set-credentials", register, {
-        credentials: { rules_to: {type: "text"}}
-    });
+    RED.nodes.registerType("set-credentials", register, {});
 }
 
 const input = (RED, node, data, config) => {
 
-    var creds = (node.credentials.rules_to) ? JSON.parse(node.credentials.rules_to) : [''];
-    var rules = config.rules;
+    var creds = (config.rules.credentials.rules_to) ? JSON.parse(config.rules.credentials.rules_to) : [''];
+    var rules = config.rules.rules;
+    var obj = {}
 
     for (let i=0; i<rules.length; i++) {
         if (!rules[i].p) continue;
-        let value = getValue(creds[i], rules[i].tot);
-
-        switch(rules[i].pt) {
-            case "flow":
-              flowContext.set(rules[i].p, value);
-              break;
-            case "global":
-              globalContext.set(rules[i].p, value);
-              break;
-            case "msg":
-            default:
-                RED.util.setMessageProperty(data, rules[i].p, value);
-          }
+        obj[rules[i].p] = getValue(creds[i], rules[i].tot);
     }
 
+    if (config.setType === "global") node.context().global.set(config.set, obj);
+    else RED.util.setMessageProperty(data, config.set, obj);
+      
 	return node.send(data);
 }
 
