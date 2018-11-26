69beae602cce105daf84d1a5e2b17bc148906a05
Klervix <klervi.menoret@gmail.com>
2018-11-26 18:38:06 +0100

    LUIS new version

--- node-red-contrib-ms-oxford/node-luis-config.js
+++ node-red-contrib-ms-oxford/node-luis-config.js
@@ -1,15 +1,18 @@
 module.exports = function(RED) {
     const register = function (config) {
         RED.nodes.createNode(this, config);
+
         this.name = config.name;
-        this.host = config.host || "https://westus.api.cognitive.microsoft.com";
         this.way = config.way;
+        this.location = config.location;
+        this.verbose = config.verbose;
+        this.staging = config.staging;
     }
     RED.nodes.registerType("ms-luis-config", register, {
     	credentials: {
-			appId:        { type: "text" },
-            subKey:       { type: "text" },
-            endpoint:     { type: "text" }
+            endpoint:     { type: "text" },
+            appId:        { type: "text" },
+            subKey:       { type: "text" }
     	}
     });
 }
