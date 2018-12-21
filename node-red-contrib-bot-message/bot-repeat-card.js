414b03b4d47a349d6293a31e6163c751be8ad36d
Klervix <klervi.menoret@gmail.com>
2018-12-21 13:57:08 +0100

    repeat

--- node-red-contrib-bot-message/bot-repeat-card.js
+++ node-red-contrib-bot-message/bot-repeat-card.js
@@ -10,14 +10,20 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         var node = this;
 
-        this.on('input', (data)  => { input(node, data, config)  });
+        this.on('input', (data)  => { input(RED, node, data, config)  });
     }
     RED.nodes.registerType("repeat-card", register, {});
 }
 
 
-const input = (node, data, config) => {
-    helper.emitAsyncEvent('repeat', node, data, config, (newData) => {
-        node.send(newData);
-    });
+const input = (RED, node, data, config) => {
+
+    if (data._replyid && data.reply) {
+        let targetNode = RED.nodes.getNode(data._replyid);
+        if (targetNode.type === 'send-card' && targetNode.repeat) {
+            targetNode.repeat(data, data.reply);
+        }
+    }
+    node.send(data);
+
 }
