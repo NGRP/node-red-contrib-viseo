45db390a716097bd44f7429fe7cba34ef1953076
Klervix <klervi.menoret@gmail.com>
2019-01-25 16:46:04 +0100

    Context

--- node-red-contrib-google-actions/google-actions.js
+++ node-red-contrib-google-actions/google-actions.js
@@ -16,39 +16,31 @@ module.exports = function(RED) {
             node.status({});
         }
 
-        this.on('input', (data)  => { input(node, data, config,) });
+        this.on('input', (data)  => { input(RED, node, data, config,) });
     }
     RED.nodes.registerType("google-actions", register, {});
 }
 
-function input (node, data, config) {
+function input (RED, node, data, config) {
     // action
     let action = config.action;
-
-    // output
-    let outloc =  (config.outputType === 'global') ? node.context().global : data;
       
     try {
         node.auth.authenticate((auth) => {
             if (auth) {
                 if (action === 'token') {
-                    helper.setByString(outloc, config.output || "payload", auth.credentials);
+                    helper.setByString(data, config.output || "payload", auth.credentials);
                     return node.send(data);
                 }
                 else {
                     // input
-                    let input = config.input;
-                    if (config.inputType === 'json') input = (typeof input === 'object') ? input : JSON.parse(input);
-                    else {
-                        let loc = (config.inputType === 'global') ? node.context().global : data;
-                        input = helper.getByString(loc, input || 'customPushMessage');
-                    }
+                    let input = helper.getContextValue(RED, node, data, config.input, config.inputType);
                     if (input.customPushMessage) input = input.customPushMessage;
                     if (!input.orderUpdate)      input = { orderUpdate: input };
 
                     send(auth, input)
                     .then( function (res) {
-                        helper.setByString(outloc, config.output || "payload", res);
+                        helper.setByString(data, config.output || "payload", res);
                         return node.send(data);
                     })
                     .catch(function (err) {
