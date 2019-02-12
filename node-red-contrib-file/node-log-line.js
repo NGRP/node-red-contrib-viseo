54ac472192eefbbbb0b1c41766462cdba6724e95
ppraud <philippe.praud@viseo.com>
2019-02-12 16:12:25 +0100

    rename function

--- node-red-contrib-file/node-log-line.js
+++ node-red-contrib-file/node-log-line.js
@@ -33,27 +33,27 @@ buildLogstr = function(shouldEncapsulate, config, logstr, separate, data, nb) {
     for (let temp of config.template) {
         switch(temp.typed) {
           case 'date':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, String(Date.now())) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, String(Date.now())) + encapsulateStr + separate;
               break;
           case 'option_date':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, (new Date()).toISOString()) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, (new Date()).toISOString()) + encapsulateStr + separate;
               break;
           case 'option_carr':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, data.user.address.carrier) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, data.user.address.carrier) + encapsulateStr + separate;
               break;
           case 'option_conv':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, data.user.address.conversation.id) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, data.user.address.conversation.id) + encapsulateStr + separate;
               break;
           case 'option_userid':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, data.user.id) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, data.user.id) + encapsulateStr + separate;
               break;
           case 'option_userna':
-              logstr += encapsulateStr + encapsulate(shouldEncapsulate, data.user.name) + encapsulateStr + separate;
+              logstr += encapsulateStr + escapeEncapsulateStr(shouldEncapsulate, data.user.name) + encapsulateStr + separate;
               break;
           case 'str':
               let cont = config.content[nb];
               logstr += encapsulateStr;
-              logstr += encapsulate(shouldEncapsulate, (cont.typed === 'msg') ? helper.getByString(data, cont.value) : cont.value);
+              logstr += escapeEncapsulateStr(shouldEncapsulate, (cont.typed === 'msg') ? helper.getByString(data, cont.value) : cont.value);
               logstr += encapsulateStr + separate ;
               nb++;
         }
@@ -62,17 +62,17 @@ buildLogstr = function(shouldEncapsulate, config, logstr, separate, data, nb) {
     return logstr;
 }
 
-encapsulate = function(shouldEncapsulate, strToEncapsulate) {
+escapeEncapsulateStr = function(shouldEncapsulate, str) {
     if (shouldEncapsulate === false) {
-        return strToEncapsulate;
-    } else if (!strToEncapsulate) {
+        return str;
+    } else if (!str) {
         return "";
     }
-  	return strToEncapsulate.replace(/"/g, '""');
+  	return str.replace(/"/g, '""');
 }
 
 const input = (node, data, config) => {
-    
+
     // Ensure output path
     let logpath = path.normalize(config.path);
     helper.mkpathsync(path.dirname(logpath));
@@ -118,7 +118,7 @@ const input = (node, data, config) => {
                     file: items[i]
                 });
             }
-            
+
             var keep = Number(config.keepFiles);
             if (files.length <= keep) return node.send(data);
             files = files.sort( function(a,b) {
@@ -134,7 +134,7 @@ const input = (node, data, config) => {
             files = files.slice(0, keep+1);
             for (let f of files) {
                 console.log("[Log lines] Deleted file " + f.file)
-                fs.unlinkSync(path.join(folder, f.file));    
+                fs.unlinkSync(path.join(folder, f.file));
             }
             return node.send(data);
         });
