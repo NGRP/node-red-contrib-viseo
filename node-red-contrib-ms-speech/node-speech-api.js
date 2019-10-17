839986a7eb72b666ac4dc8e0dde670fb2ea7bc8c
Jean-Philippe Encausse <jean-philippe.encausse@viseo.com>
2019-10-17 19:34:17 +0200

    Fix ms-speech node with new API feature tts and stt

--- node-red-contrib-ms-speech/node-speech-api.js
+++ node-red-contrib-ms-speech/node-speech-api.js
@@ -37,31 +37,35 @@ async function input (node, data, config) {
     if (config.inputType === "msg") input = helper.getByString(data, input);
     if (token && config.tokenType === "msg") token = helper.getByString(data, token);
 
+    parameters.region = config.key.region || 'westus'
 
+    // 1. Get Access Token
+    if (!token) {
+        tryToken = true;
+        try {
+            token = await getToken(config.key.key, parameters.region);
+
+            let voices = await getVoices(parameters, token);
+            parameters.voices = JSON.parse(voices.toString())
+            node.warn(parameters.voices)
+        }
+        catch(err) {
+            node.error(err);
+            return node.send(data);
+        }
+    }
 
     if (api === 'stt') {
-
-        // 1. Get Access Token
-        if (!token) {
-            tryToken = true;
-            try {
-                token = await getToken(config.key.key, config.key.region);
-            }
-            catch(err) {
-                node.error(err);
-                return node.send([null, data]);
-            }
-        }
-
+       
         // 2. Prepare request
-        let language =    (config.languageType === 'msg') ?     helper.getByString(data, config.language) : config.language;
+        let language    = (config.languageType === 'msg') ?  helper.getByString(data, config.language) : config.language;
         let contentType = (config.contentTypeType === 'msg') ?  helper.getByString(data, config.contentType) : config.contentType;
 
         if (typeof input === "string") input = fs.readFileSync(input);
 
-        parameters.language =    language || 'fr-FR';
+        parameters.language    = language || 'fr-FR';
         parameters.contentType = contentType || 'audio/wav; codec="audio/pcm"; samplerate=16000';
-        
+
         // 3. Send request
         try {
             let result = await STT(input, parameters, token);
@@ -70,7 +74,7 @@ async function input (node, data, config) {
 
             // 4. Send result
             helper.setByString(data, output, {token: token, result: result});
-            return node.send([data, null]);
+            return node.send(data);
         }
         catch (err) {
             // 4 bis. Get a new token and retry
@@ -83,66 +87,53 @@ async function input (node, data, config) {
                     catch(ex) { node.warn('JSON Parse Exception: ' + result) }
         
                     helper.setByString(data, output, {token: token, result: result});
-                    return node.send([data, null]);
+                    return node.send(data);
                 }
                 catch(err) {
                     node.error(err);
-                    return node.send([null, data]);
+                    return node.send(data);
                 }
             }
             node.error(err);
-            return node.send([null, data]);
+            return node.send(data);
         }
     }
     else {
-         // 1. Prepare request
+
+         // 2. Prepare request
         let userAgent = (config.userAgentType === 'msg') ?     helper.getByString(data, config.userAgent) : config.userAgent;
         let outFormat = (config.outputFormatType === 'msg') ?  helper.getByString(data, config.outputFormat) : config.outputFormat;
         let region =    (config.regionType === 'msg') ?        helper.getByString(data, config.region) : config.region;
 
-        parameters["X-Microsoft-OutputFormat"] = outFormat || "ssml-16khz-16bit-mono-tts";
-        parameters["region"] = region || "westus";
+        parameters["region"] = region || parameters.region;
         parameters["User-Agent"] = userAgent || undefined;
-
-        // 2. Get Access Token
-        if (!token) {
-            tryToken = true;
-            try {
-                token = await getToken(config.key.key, region);
-            }
-            catch(err) {
-                node.error(err);
-                return node.send([null, data]);
-            }
-        }
+        parameters["X-Microsoft-OutputFormat"] = outFormat || "riff-24khz-16bit-mono-pcm";
 
          // 3. Send request
          try {
-             let result = await TTS(input, parameters, token);
-             helper.setByString(data,config.output || 'payload', {token: token, result: result});
-
-             return node.send([data, null]);
+            let result = await TTS(input, parameters, token);
+            helper.setByString(data,config.output || 'payload', {token: token, result: result});
+            return node.send(data);
          }
          catch (err) {
-             // 4 bis. Get a new token and retry
-             node.warn(err)
-             if (typeof(err) === "string" && err.match(/403/) && !tryToken) {
-                 tryToken = true;
-                 try {
+            // 4 bis. Get a new token and retry
+            node.warn(err)
+            if (typeof(err) === "string" && err.match(/403/) && !tryToken) {
+                tryToken = true;
+                try {
                     token = await getToken(config.key.key, parameters.region);
-                    let result = await TTS(input, parameters, token);        
+                    let result = await TTS(input, parameters, token);
                     helper.setByString(data,config.output || 'payload', {token: token, result: result});
-                    return node.send([data, null]);
-                 }
-                 catch(err) {
+                    return node.send(data);
+                }
+                catch(err) {
                     node.error(err);
-                    return node.send([null, data]);
-                 }
-             }
+                    return node.send(data);
+                }
+            }
              node.error(err);
-             return node.send([null, data]);
+             return node.send(data);
          }
-         
     }
 }
 
@@ -156,15 +147,13 @@ async function getToken(key, region) {
             'Content-Length': 0
         }
     }
-
     return rp(auth);
 }
 
-
-
-
 async function STT(input, parameters, token) {
-    let URI = 'https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?';
+    
+    // let URI = 'https://speech.platform.bing.com/speech/recognition/interactive/cognitiveservices/v1?';
+    let URI = 'https://' + parameters.region + '.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?'
     let QS  = 'language='+ parameters.language +'&format=simple&requestid=node-red-viseo';
 
     let req  = { 
@@ -177,11 +166,19 @@ async function STT(input, parameters, token) {
         },
         body: input
     }
-
     return rp(req);
 }
 
 async function TTS(input, parameters, token) {
+
+    let ssml = input || ''
+    if (ssml.indexOf('<') !== 0) {
+        ssml = "<speak version='1.0' xml:lang='"+(parameters["ttsLang"] || 'fr-FR')+"'>"
+             + "<voice xml:lang='fr-FR' xml:gender='"+(parameters["ttsGender"] || 'Female')+"' name='"+(parameters["ttsName"] || 'fr-FR-Julie-Apollo')+"'>"
+             + input
+             + "</voice></speak>"
+    }
+
     let URI = 'https://' + parameters.region + '.tts.speech.microsoft.com/cognitiveservices/v1';
     let req  = { 
         url: URI,
@@ -192,9 +189,21 @@ async function TTS(input, parameters, token) {
             'User-Agent': parameters["User-Agent"],
             'X-Microsoft-OutputFormat' : parameters["X-Microsoft-OutputFormat"]
         },
-        body: input,
+        body: ssml,
         encoding: null
     }
+    return rp(req);
+}
 
+async function getVoices(parameters, token) {
+    let URI = 'https://' + parameters.region + '.tts.speech.microsoft.com/cognitiveservices/voices/list';
+    let req  = { 
+        url: URI,
+        method: 'GET',
+        headers: { 
+            'Authorization': 'Bearer ' + token
+        },
+        encoding: null
+    }
     return rp(req);
 }
