0a81bff19f5d270ec181517caebadb7809a1bfb7
Siyao D <siyao.dong@gmail.com>
2020-12-10 16:04:16 +0100

    send welcome message when there is new user

--- node-red-contrib-botbuilder/msbot/botVBM.js
+++ node-red-contrib-botbuilder/msbot/botVBM.js
@@ -3,24 +3,20 @@ const { ActivityHandler } = require("botbuilder");
 class VBMBot extends ActivityHandler {
   constructor(node, welcomeMessage, sendWelcomeMessage) {
     super();
-
-    // If a welcome message is defined, handle the new user event
-    if (welcomeMessage) {
-      console.log("WELCOME", welcomeMessage)
-      this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-          if (membersAdded[cnt].id === context.activity.recipient.id) {
-            return await new Promise(function(resolve, reject) {
-              context.activity.type = "message";
-              context.activity.value = welcomeMessage;
-              sendWelcomeMessage(node, context, resolve, reject, next);
-            });
-          }
+      
+    this.onMembersAdded(async (context, next) => {
+      const membersAdded = context.activity.membersAdded;
+      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        if (membersAdded[cnt].id === context.activity.recipient.id) {
+          return await new Promise(function(resolve, reject) {
+            context.activity.type = "message";
+            context.activity.value = welcomeMessage;
+            sendWelcomeMessage(node, context, resolve, reject, next);
+          });
         }
-        await next();
-      });
-    }
+      }
+      await next();
+    });
   }
 }
 
