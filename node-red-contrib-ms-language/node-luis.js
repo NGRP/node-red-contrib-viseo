c2bdc3b4812bf76e24b5dbde428c20406c9e7caa
Siyao D <siyao.dong@gmail.com>
2021-09-09 11:25:43 +0200

    upgrade LUIS API to v3

--- node-red-contrib-ms-language/node-luis.js
+++ node-red-contrib-ms-language/node-luis.js
@@ -1,6 +1,59 @@
 const request =    require('request-promise');
 const helper     = require('node-red-viseo-helper');
 
+const asyncGetPrediction = async function(node, text) {
+    const response = await request({
+        uri: `${node.endpoint}${encodeURIComponent(text)}`,
+        method: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': node.subKey,
+            ...(node.spellCheckKey && {'mkt-bing-spell-check-key': node.spellCheckKey})
+        },
+        json: true
+    });
+
+    const entities = Object.values(response.prediction.entities.$instance)
+        .reduce((result, entity) => result.concat(...entity), []) // flat entity array
+        .map(entity => {
+            return {
+                ...entity,
+                resolutionValues: response.prediction.entities[entity.type].reduce((result, entity) => result.concat(...entity), []) // add resolution values
+            }
+        })
+
+    return {
+        query: response.query,
+        alteredQuery: response.prediction.alteredQuery,
+        intents: response.prediction.intents,
+        entities,
+        topScoringIntent: {
+            intent: response.prediction.topIntent,
+            score: response.prediction.intents[response.prediction.topIntent].score
+        },
+        source: 'luis'
+    };
+}
+
+
+const input = function(RED, node, data, config) {
+    // Log activity
+    try { setTimeout(function() { helper.trackActivities(node)},0); }
+    catch(err) { console.log(err); }
+
+    // Get parameters
+    let output = config.intent || 'payload';
+    let text = helper.getContextValue(RED, node, data, config.text || 'payload', config.textType);
+
+    asyncGetPrediction(node, text).then(response => {
+        helper.setByString(data, output, response);
+        node.send(data);
+    }).catch(error => {
+        console.error(error);
+        node.error({ message: error.message, source: 'ms-luis' });
+		node.send(data);
+    });
+}
+
 // https://github.com/Microsoft/Cognitive-LUIS-Node.js
 // --------------------------------------------------------------------------
 //  NODE-RED
@@ -12,61 +65,21 @@ module.exports = function(RED) {
 
         let node = this;
         let conf = RED.nodes.getNode(config.config);
-        node.status({fill:"red", shape:"ring", text: 'Missing credentials'});
-        
+        node.status({fill:'red', shape:'ring', text: 'Missing credentials'});
+
         if (!conf || !conf.credentials) return;
-        if (conf.way === "key") {
-            this.endpoint  = "https://" + (conf.location || "westus") + ".api.cognitive.microsoft.com/luis/v2.0/apps/" + conf.credentials.appId;
-            this.endpoint += "?subscription-key=" + conf.credentials.subKey + (conf.verbose ? "&verbose=" + conf.verbose : "") 
-            this.endpoint += (conf.staging ? "&staging=" + conf.staging : "") + "&q=";
+        if (conf.way === 'key') {
+            // Endpoint URL changes for V3
+            this.endpoint = `https://${conf.location}.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/${conf.credentials.appId}/slots/${conf.staging ? 'staging' : 'production'}/predict?verbose=${conf.verbose ? 'true' : 'false'}&log=true&show-all-intents=true&query=`;
+            this.subKey = conf.credentials.subKey;
+            if (conf.credentials.spellCheckKey && conf.credentials.spellCheckKey !== '') {
+                this.spellCheckKey = conf.credentials.spellCheckKey;
+            }
         }
         else this.endpoint = conf.credentials.endpoint;
 
         if (this.endpoint) node.status({});
-        this.on('input', (data)  => { try { input(RED, node, data, config) } catch (ex){ node.warn(ex) }});
+        this.on('input', (data)  => { input(RED, node, data, config) });
     }
-    RED.nodes.registerType("ms-luis", register, {});
-}
-
-
-async function input(RED, node, data, config) {
-
-    // Log activity
-    try { setTimeout(function() { helper.trackActivities(node)},0); }
-    catch(err) { console.log(err); }
-
-
-    // Get parameters
-    let url = node.endpoint;  
-    let output = config.intent || "payload";
-    let text = helper.getContextValue(RED, node, data, config.text || "payload", config.textType);
-    
-    try {
-        let response = await request({
-            uri: url + encodeURIComponent(text),
-            method: 'GET'
-        });
-
-        response = JSON.parse(response);
-
-        let formattedResponse = {
-            query: response.query,
-            intent: response.topScoringIntent.intent,
-            score: response.topScoringIntent.score,
-            entities: {},
-            source: "luis",
-            completeResponse: response
-        }
-
-        for (let e of response.entities) {
-            if (!e.type) continue;
-            formattedResponse.entities[e.type] = e.entity;
-        }
-        
-        helper.setByString(data, output, formattedResponse);
-        return node.send(data);
-    }
-    catch(err) { 
-        return node.error(err);
-    }
+    RED.nodes.registerType('ms-luis', register, {});
 }
