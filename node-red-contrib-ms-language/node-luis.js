63b98dd3184664ab643a0d5491d0179632e874de
Siyao D <siyao.dong@gmail.com>
2021-09-28 17:18:33 +0200

    refactoring

--- node-red-contrib-ms-language/node-luis.js
+++ node-red-contrib-ms-language/node-luis.js
@@ -5,47 +5,50 @@ const extractEntities = (prediction) => {
     const entities = [];
     const compositeEntities = [];
 
-    Object.values(prediction.entities.$instance)
-    .reduce((result, entity) => result.concat(...entity), []) // flat entity array
-    .forEach(entity => {
-        if (!entity) return;
-        if (entity.modelType === 'Composite Entity Extractor') {
-            compositeEntities.push({
-                parentType: entity.type,
-                value: entity.text,
-                children: []
-            });
-            Object.values(prediction.entities[entity.type][0].$instance).forEach(child => {
-                compositeEntities[compositeEntities.length - 1].children.push({
-                  type: child[0].type,
-                  value: child[0].text
+    // Merge additional metadata in the $instance object to entities
+    if (prediction.entities.$instance) {
+        Object.values(prediction.entities.$instance)
+        .reduce((result, entity) => result.concat(...entity), []) // flat entity array
+        .forEach(entity => {
+            if (!entity) return;
+            if (entity.modelType === 'Composite Entity Extractor') {
+                compositeEntities.push({
+                    parentType: entity.type,
+                    value: entity.text,
+                    children: []
                 });
+                Object.values(prediction.entities[entity.type][0].$instance).forEach(child => {
+                    compositeEntities[compositeEntities.length - 1].children.push({
+                      type: child[0].type,
+                      value: child[0].text
+                    });
+                    entities.push({
+                      type: child[0].type,
+                      entity: child[0].text,
+                      startIndex: child[0].startIndex,
+                      endIndex: child[0].startIndex + child[0].length,
+                      score: entity.score
+                    })
+                  });
+            } else {
                 entities.push({
-                  type: child[0].type,
-                  entity: child[0].text,
-                  startIndex: child[0].startIndex,
-                  endIndex: child[0].startIndex + child[0].length,
-                  score: entity.score
-                })
-              });
-        } else {
-            entities.push({
-                type: entity.type,
-                entity: entity.text,
-                startIndex: entity.startIndex,
-                endIndex: entity.startIndex + entity.length, // add end index
-                resolution: {
-                  values: response.prediction.entities[entity.type.replace('builtin.', '')].reduce((result, e) => {
-                    if (Array.isArray(e)) {
-                      return result.concat(...e);
+                    type: entity.type,
+                    entity: entity.text,
+                    startIndex: entity.startIndex,
+                    endIndex: entity.startIndex + entity.length, // add end index
+                    resolution: {
+                      values: prediction.entities[entity.type.replace('builtin.', '')].reduce((result, e) => {
+                        if (Array.isArray(e)) {
+                          return result.concat(...e);
+                        }
+                        result.push(e);
+                        return result;
+                      }, [])
                     }
-                    result.push(e);
-                    return result;
-                  }, [])
-                }
-            });
-        }
-    });
+                });
+            }
+        });
+    }
 
     return {
         entities,
@@ -53,26 +56,21 @@ const extractEntities = (prediction) => {
     };
 };
 
-const asyncGetPrediction = async function(node, text) {
-    let entityObject = {
-        entities: [],
-        compositeEntities: []
+const getPrediction = async function(node, text) {
+    const headers = {
+        'Ocp-Apim-Subscription-Key': node.subKey
     };
-
+    if (node.spellCheckKey) {
+        headers['mkt-bing-spell-check-key'] = node.spellCheckKey;
+    }
     const response = await request({
         uri: `${node.endpoint}${encodeURIComponent(text)}`,
         method: 'GET',
-        headers: {
-            'Ocp-Apim-Subscription-Key': node.subKey,
-            ...(node.spellCheckKey && {'mkt-bing-spell-check-key': node.spellCheckKey})
-        },
+        headers,
         json: true
     });
 
-    // Merge additional metadate in the $instance object to entities
-    if (response.prediction.entities.$instance) {
-        entityObject = extractEntities(response.prediction);
-    }
+    const entityObject = extractEntities(response.prediction);
 
     return {
         query: response.query,
@@ -89,7 +87,7 @@ const asyncGetPrediction = async function(node, text) {
 }
 
 
-const input = function(RED, node, data, config) {
+const input = async function(RED, node, data, config) {
     // Log activity
     try { setTimeout(function() { helper.trackActivities(node)},0); }
     catch(err) { console.log(err); }
@@ -98,14 +96,14 @@ const input = function(RED, node, data, config) {
     let output = config.intent || 'payload';
     let text = helper.getContextValue(RED, node, data, config.text || 'payload', config.textType);
 
-    asyncGetPrediction(node, text).then(response => {
-        helper.setByString(data, output, response);
+    try {
+        const prediction = await getPrediction(node, text);
+        helper.setByString(data, output, prediction);
         node.send(data);
-    }).catch(error => {
+    } catch (error) {
         console.error(error);
         node.error({ message: error.message, source: 'ms-luis' });
-		node.send(data);
-    });
+    }
 }
 
 // https://github.com/Microsoft/Cognitive-LUIS-Node.js
@@ -126,14 +124,24 @@ module.exports = function(RED) {
             // Endpoint URL changes for V3
             this.endpoint = `https://${conf.location}.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/${conf.credentials.appId}/slots/${conf.staging ? 'staging' : 'production'}/predict?verbose=${conf.verbose ? 'true' : 'false'}&log=true&show-all-intents=true&query=`;
             this.subKey = conf.credentials.subKey;
-            if (conf.credentials.spellCheckKey && conf.credentials.spellCheckKey !== '') {
-                this.spellCheckKey = conf.credentials.spellCheckKey;
-            }
+        } else {
+            this.endpoint = conf.credentials.endpoint;
         }
-        else this.endpoint = conf.credentials.endpoint;
+
+        // Spell check key
+        if (conf.credentials.spellCheckKey && conf.credentials.spellCheckKey !== '') {
+            this.spellCheckKey = conf.credentials.spellCheckKey;
+        }
 
         if (this.endpoint) node.status({});
-        this.on('input', (data)  => { input(RED, node, data, config) });
+        this.on('input', (data)  => {
+            try {
+                await input(RED, node, data, config);
+            } catch (error) {
+                console.error(error);
+                node.error({ message: error.message, source: 'ms-luis' });
+            }
+        });
     }
     RED.nodes.registerType('ms-luis', register, {});
 }
