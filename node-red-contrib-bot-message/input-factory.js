6e2624d55187118e7234bd4d78d01ada89f2cc45
Siyao D <siyao.dong@gmail.com>
2021-10-13 16:21:45 +0200

    checkbox, radiobutton, text input

--- node-red-contrib-bot-message/input-factory.js
+++ node-red-contrib-bot-message/input-factory.js
@@ -1,15 +1,44 @@
 const CARD_CONST = require('./constants.js');
 
-const buildCheckbox = () => {
-    return;
+const buildCheckbox = (label, $data) => {
+    // "isMultiSelect": true,
+    return {
+        type: CARD_CONST.CHOICE_SET,
+        label,
+        isMultiSelect: $data.isMultiSelect ? $data.isMultiSelect : true,
+        value: "1",
+        choices: [
+            {
+                $data,
+                title: "${choice}",
+                value: "${value}"
+            }
+        ]
+    };
 };
 
-const buildRadioButton = () => {
-    return;
+const buildRadioButton = (label, $data) => {
+    return {
+        type: CARD_CONST.CHOICE_SET,
+        label,
+        style: "expanded",
+        value: "1",
+        choices: [
+            {
+                $data,
+                title: "${choice}",
+                value: "${value}"
+            }
+        ]
+    };
 };
 
-const buildTextblock = () => {
-    return;
+const buildTextblock = (label) => {
+    return {
+        type: "Input.Text",
+        label,
+        style: "text"
+    };
 };
 
 const buildDropdown = (label, $data) => {
