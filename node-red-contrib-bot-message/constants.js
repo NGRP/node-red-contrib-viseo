31eabb026e468731b60a2b685bd9c4d9b6d5201c
Siyao D <siyao.dong@gmail.com>
2021-10-22 11:34:12 +0200

    action submit (part2)

--- node-red-contrib-bot-message/constants.js
+++ node-red-contrib-bot-message/constants.js
@@ -1,6 +1,14 @@
 module.exports = {
-    TEXT_BLOCK: "Input.Text",
-    CHOICE_SET: "Input.ChoiceSet",
+    CARD_ADAPTIVECARD: "AdaptiveCard",
+    TEXT_BLOCK: "TextBlock",
+    INPUT_TEXT: "Input.Text",
+    INPUT_CHOICESET: "Input.ChoiceSet",
     STYLE_COMPACT: "compact",
-    STYLE_EXPANDED: "expanded"
+    STYLE_EXPANDED: "expanded",
+    ACTION_SUBMIT: "Action.Submit",
+    STYLE_EXPANDED: "expanded",
+    TYPE_CHECKBOX: "checkbox",
+    TYPE_RADIOBUTTON: "radiobutton",
+    TYPE_DROPDOWN: "dropdownlist",
+    TYPE_TEXT: "text"
 };
--- node-red-contrib-bot-message/input-factory.js
+++ node-red-contrib-bot-message/input-factory.js
@@ -1,44 +1,44 @@
 const CARD_CONST = require('./constants.js');
 
-const buildCheckbox = (label, $data) => {
+const buildCheckbox = (id, index, label, $data) => {
     return {
-        id: `checkbox${new Date().getTime()}`,
-        type: CARD_CONST.CHOICE_SET,
+        id: `${id}_${CARD_CONST.TYPE_CHECKBOX}_${index}`,
+        type: CARD_CONST.INPUT_CHOICESET,
         label,
         isMultiSelect: $data.isMultiSelect ? $data.isMultiSelect : true,
-        value: $data.value ? $data.value : "1",
+        value: $data.value ? $data.value : "1", // "1" means the first option of the checkbox
         choices: $data.choices
     };
 };
 
-const buildRadioButton = (label, $data) => {
+const buildRadioButton = (id, index, label, $data) => {
     return {
-        id: `radiobutton${new Date().getTime()}`,
-        type: CARD_CONST.CHOICE_SET,
+        id: `${id}_${CARD_CONST.TYPE_RADIOBUTTON}_${index}`,
+        type: CARD_CONST.INPUT_CHOICESET,
         label,
         style: CARD_CONST.STYLE_EXPANDED,
-        value: "1",
+        value: "1", // "1" means the first option of the radio buttons
         choices: $data.choices
     };
 };
 
-const buildTextblock = (label) => {
+const buildTextblock = (id, index, label) => {
     return {
-        id: `textblock${new Date().getTime()}`,
-        type: CARD_CONST.TEXT_BLOCK,
+        id: `${id}_${CARD_CONST.TYPE_TEXT}_${index}`,
+        type: CARD_CONST.INPUT_TEXT,
         label,
-        style: "text",
+        style: CARD_CONST.TYPE_TEXT,
         isMultiline: true
     };
 };
 
-const buildDropdown = (label, $data) => {
+const buildDropdown = (id, index, label, $data) => {
  return {
-        id: `dropdown${new Date().getTime()}`,
-        type: CARD_CONST.CHOICE_SET,
+        id: `${id}_${CARD_CONST.TYPE_DROPDOWN}_${index}`,
+        type: CARD_CONST.INPUT_CHOICESET,
         label,
         style: CARD_CONST.STYLE_COMPACT,
-        value: $data.value ? $data.value : "1",
+        value: $data.value ? $data.value : "1", // "1" means the first option of the dropdown list
         choices: $data.choices
     };
 };
