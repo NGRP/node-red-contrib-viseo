763fb9671f3c93aadcf29ee5613b5e72005dfe77
AzureAD\RemyRuzafa <remy.ruzafa@viseo.com>
2021-11-23 10:24:20 +0100

    feedback FAQ

--- node-red-contrib-botbuilder/msbot/botVBM.js
+++ node-red-contrib-botbuilder/msbot/botVBM.js
@@ -42,7 +42,15 @@ class VBMBot extends ActivityHandler {
       await next();
     });
   }
-
+  async onReactionsAddedActivity(reactionsAdded, context) {
+    for (var i = 0, len = reactionsAdded.length; i < len; i++) {
+        context.activity.type = "messageReaction";
+        context.activity.value = context.activity.reactionsAdded;
+        context.activity.reactionsAdded = null;
+        this.sendWelcomeMessage(this.node, context);
+     }
+  };
+  
   // Override the ActivityHandler.run() method to save state changes
   async run(context) {
     await super.run(context);
